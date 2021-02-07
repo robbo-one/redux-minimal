3a192d2020b5c737c3d7c068d23ee51c323f26ab
student <student@eda-bc-06.localdomain>
2021-02-08 11:26:29 +1300

    added delete func

--- client/components/Wombats.jsx
+++ client/components/Wombats.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Wombats (props) {
   const {store} = props
   const state = store.getState()
   const wombats = state.wombats
+
+  const [formData, setFormData] = useState({})
+
+  const handleChange = (event) => {
+    event.persist()
+    setFormData(currentData => {
+      return {
+        ...currentData,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const action = {
+      type: 'UPDATE_WOMBAT',
+      wombat: 'Gertrude',
+      newWombat: 'Gerty'
+    }
+    store.dispatch(action)
+  }
+
+  const handleDelete = (event, wombat) => {
+    event.preventDefault()
+    const action = {
+      type: 'DEL_WOMBAT',
+      wombat: wombat
+    }
+    store.dispatch(action)
+  }
+
   return (
     <div>
       <h1>Wombats</h1>
       <ul>
-        {wombats.map((wombat) => <li key={wombat}>{wombat}</li>)}
+        {wombats.map((wombat) => <li key={wombat}>{wombat}<button onClick={(event) => handleDelete(event, wombat)}>Delete</button></li>)}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Update Wombat:
+          <input type='text' name='newWombat' onChange={(event) => handleChange(event)} />
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
--- client/reducer.js
+++ client/reducer.js
@@ -12,6 +12,16 @@ const wombatReducer = (state = initialWombatState, action) => {
       return {
         wombats: state.wombats.filter((wombat) => wombat !== action.wombat)
       }
+    case 'UPDATE_WOMBAT':
+      return {
+        wombats: state.wombats.map((wombat) => {
+          if (wombat === action.wombat) {
+            return action.newWombat
+          } else {
+            return wombat
+          }
+        }) 
+      }
     default:
       return state
   }
