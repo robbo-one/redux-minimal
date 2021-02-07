be698f9ab929fe54af7b451a3e2e429cd083aba6
student <student@eda-bc-03.localdomain>
2021-02-08 10:22:14 +1300

    update wombat

--- client/reducer.js
+++ client/reducer.js
@@ -12,9 +12,18 @@ const wombatReducer = (state = initialWombatState, action) => {
       return {
         wombats: state.wombats.filter((wombat) => wombat !== action.wombat)
       }
+    case 'UPDATE_WOMBAT':
+      // const w = wombats.find(wombat => wombat == action.wombat)
+      return {
+        wombats: state.wombats.map(wombat => {
+          console.log(wombat)
+         return wombat === action.oldWombat ? action.newWombat : wombat
+        })
+      }
     default:
       return state
   }
 }
 
+// wombat == action.oldWombat && action.newWombat
 export default wombatReducer
