c3297445b204864b27d0f965e6ff9720569205dd
Rob <robsc801@gmail.com>
2021-02-08 11:26:32 +1300

    "delete a wombat working

--- client/index.js
+++ client/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore } from 'redux' // import this to use REdux
 
 import App from './components/App'
 import wombatReducer from './reducer'
 
-const store = createStore(wombatReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(wombatReducer,//we call it here, store has the event bus 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
 )
 
 document.addEventListener('DOMContentLoaded', () => {
   render()
-  store.subscribe(render)
+  store.subscribe(render) // this subscribes to changes and anytime something changes, it calls the render function below and renders the page again
 })
 
 function render () {
   ReactDOM.render(
-    <App store={store} />,
+    <App store={store} />, //REdux is the store
     document.getElementById('app')
   )
 }
--- client/reducer.js
+++ client/reducer.js
@@ -1,20 +1,53 @@
 const initialWombatState = {
-  wombats: ['Gertrude', 'Bartholemew']
+  wombats: ['Gertrude', 'Bartholemew']  // initial global state
 }
+
+
+// a reducer is just a function. heres what state should look like after the function has gone through.
+
 
 const wombatReducer = (state = initialWombatState, action) => {
   switch (action.type) {
     case 'ADD_WOMBAT':
       return {
-        wombats: [...state.wombats, action.wombat]
+        wombats: [...state.wombats, action.wombat] // spread operator, take existing wombats and add the wombat to existing
       }
     case 'DEL_WOMBAT':
       return {
         wombats: state.wombats.filter((wombat) => wombat !== action.wombat)
       }
-    default:
+      case 'UPDATE_WOMBAT':
+        return {
+          wombats: state.wombats.map((wombat) => {
+            if (wombat === action.wombat) {
+            return action.newWombat
+          } else {
+            return wombat
+            }
+          })
+         
+        }
+    default: // if neither returns to default state
       return state
   }
 }
 
 export default wombatReducer
+
+
+//could also be written using if/else statements
+
+// const wombatReducer = (state = initialWombatState, action) => {
+//   if  (action.type) {
+//     case 'ADD_WOMBAT':
+//       return {
+//         wombats: [...state.wombats, action.wombat] // spread operator, take existing wombats and add the wombat to existing
+//       }
+//     else if (action.type === 'DEL_WOMBAT') {}
+//       return {
+//         wombats: state.wombats.filter((wombat) => wombat !== action.wombat)
+//       }
+//     else { // if neither returns to default state
+//       return state
+//   }
+// }
