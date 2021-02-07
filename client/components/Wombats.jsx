2e8517962891a7358b1608733c1a7d4ade38091a
student <student@eda-bc-03.localdomain>
2021-02-08 11:12:16 +1300

    update wombat button

--- client/components/Wombats.jsx
+++ client/components/Wombats.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react"
 
-function Wombats (props) {
-  const {store} = props
+function Wombats(props) {
+  const { store } = props
   const state = store.getState()
   const wombats = state.wombats
 
   const deleteWombat = (wombat) => {
     store.dispatch({
-      type: 'DEL_WOMBAT',
-      wombat: wombat
+      type: "DEL_WOMBAT",
+      wombat: wombat,
     })
   }
 
   const addWombat = (evt) => {
     evt.preventDefault()
     store.dispatch({
-      type: 'ADD_WOMBAT',
-      wombat: evt.target.newWombat.value
+      type: "ADD_WOMBAT",
+      wombat: evt.target.newWombat.value,
+    })
+  }
+
+  const updateWombat = (wombat, evt) => {
+    evt.preventDefault()
+    // console.log(wombat, evt.target)
+    store.dispatch({
+      type: 'UPDATE_WOMBAT',
+      oldWombat: wombat,
+      newWombat: evt.target.newWombat.value
     })
   }
 
@@ -24,8 +34,27 @@ function Wombats (props) {
     <div>
       <h1>Wombats</h1>
       <ul>
-        {wombats.map((wombat) => <li key={wombat}>{wombat} <button onClick={() => deleteWombat(wombat)}>Delete</button></li>)}
+        {wombats.map((wombat) => (
+          <li key={wombat}>
+            {wombat}{" "}
+            <button onClick={() => deleteWombat(wombat)}>Delete</button>{" "}
+            <form onSubmit={(evt) => updateWombat(wombat, evt)}>
+              <label>
+                Update {wombat}:  
+                <input
+                  type="text"
+                  name="newWombat"
+                  placeholder="new name"
+                ></input>
+              </label>
+              <button>Update!</button>
+            </form>
+          </li>
+        ))}
       </ul>
+
+
+
       <form onSubmit={addWombat}>
         <label>
           Add a Wombat! <br></br>
@@ -33,7 +62,6 @@ function Wombats (props) {
         </label>
         <button>Add!</button>
       </form>
-
     </div>
   )
 }
