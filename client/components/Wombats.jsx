47c2880925141f6a456ecd67f287444c14605243
student <student@eda-bc-01.localdomain>
2021-02-08 12:01:30 +1300

    Add Delete a Wombat function

--- client/components/Wombats.jsx
+++ client/components/Wombats.jsx
@@ -5,11 +5,21 @@ function Wombats (props) {
   const state = store.getState()
   const wombats = state.wombats
 
+  function deleteWombat(wombat) {
+    const action = {
+      type: 'DEL_WOMBAT',
+      wombat: wombat
+    }
+    store.dispatch(action) 
+  }
+
   return (
     <div>
       <h1>Wombats</h1>
       <ul>
-        {wombats.map((wombat) => <li key={wombat}>{wombat}</li>)}
+        {wombats.map((wombat) => <li key={wombat}>{wombat}
+          <button onClick={() => deleteWombat (wombat)}>Delete</button>
+        </li>)}
       </ul>
     </div>
   )
