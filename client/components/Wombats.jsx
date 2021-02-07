25eb6098628496f787d0b83284f10b67858d106d
student <student@eda-bc-03.localdomain>
2021-02-08 10:37:56 +1300

    add wombat form

--- client/components/Wombats.jsx
+++ client/components/Wombats.jsx
@@ -12,12 +12,28 @@ function Wombats (props) {
     })
   }
 
+  const addWombat = (evt) => {
+    evt.preventDefault()
+    store.dispatch({
+      type: 'ADD_WOMBAT',
+      wombat: evt.target.newWombat.value
+    })
+  }
+
   return (
     <div>
       <h1>Wombats</h1>
       <ul>
         {wombats.map((wombat) => <li key={wombat}>{wombat} <button onClick={() => deleteWombat(wombat)}>Delete</button></li>)}
       </ul>
+      <form onSubmit={addWombat}>
+        <label>
+          Add a Wombat! <br></br>
+          <input type="text" name="newWombat" placeholder="new wombat"></input>
+        </label>
+        <button>Add!</button>
+      </form>
+
     </div>
   )
 }
