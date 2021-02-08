76da3f71200f4de1b0f5a3b492384f460e65a97e
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-08 15:23:08 +1300

    Delete a Wombat is working

--- client/components/Wombats.jsx
+++ client/components/Wombats.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import wombatReducer from '../reducer'
 
 function Wombats (props) {
   console.log(props)
@@ -7,19 +8,24 @@ function Wombats (props) {
   const state = store.getState()
   //getState returns the current state tree of your application. It's equal to the last value returned by the store's reducer.
   const wombats = state.wombats
+
+  function deleteWombat (wombat) {
+    const action = {
+      type: 'DEL_WOMBAT',
+      wombat: wombat
+    }
+    store.dispatch(action)
+  }
+
   return (
     <div>
       <h1>Wombats</h1>
       <ul>
-        {wombats.map((wombat) => <li key={wombat}>{wombat}</li>)}
+        {wombats.map((wombat) => <li key={wombat}>{wombat}   
+        <button onClick={() => deleteWombat(wombat)} >Delete</button>
+         
+         </li>)}  
       </ul>
-      {/* <button onClick={() => {
-        const action = {
-          type: 'ADD_WOMBAT',
-          wombat: 'Hannah'
-        }
-        store.dispatch(action)
-      }}>Click me!</button> */}
     </div>
   )
 }
